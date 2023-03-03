3bf984b36bac21346d2ce645994f10037cba2bab
Joe Gaebel <joe.gaebel@hotmail.com>
2023-03-03 18:50:13 +1100

    Use proxy package

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,15 @@
-import express, {Express, Request, Response} from "express";
-import fetch from 'node-fetch';
+import express, {Express} from "express";
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import proxy from "express-http-proxy";
 
 function createApp(): Express {
     const app = express();
+
     app.use(cors());
-    app.use(bodyParser.text({type: '*/*'}));
-    app.use(express.json());
 
-    app.post('/track', async (request: Request, response: Response) => {
-        // @ts-ignore
-        const queryParams = new URLSearchParams(request.query);
-        const requestHeaders = request.headers as HeadersInit;
-        const userIp = Array.isArray(request.headers['x-forwarded-for']) ?
-            request.headers['x-forwarded-for'][0] :
-            request.headers['x-forwarded-for']!.split(',')[0];
-
-        const mixpanelResponse = await fetch(`https://api-js.mixpanel.com/track/?${queryParams}`, {
-            method: 'post',
-            body: request.body,
-            // @ts-ignore
-            headers: {
-                ...requestHeaders,
-                "X-Real-IP": userIp,
-                "x-forwarded-for": userIp,
-                "X-Forwarded-Host": "mixpanel-proxy"
-            } as unknown as Headers
-        })
-
-        const mixPanelBody = await mixpanelResponse.json();
-
-        response
-            .status(mixpanelResponse.status)
-            .send(mixPanelBody);
-    });
+    app.use('/', proxy('https://api-js.mixpanel.com', {
+        https: true,
+    }));
 
     return app;
 }
